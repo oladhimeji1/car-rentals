e5c32f4728dfb77e7d923110446d1e6b876faf05
Mohammed Oladimeji Abdulrazaq <abdulrazaq@gmail.com>
2023-11-13 12:33:49 +0100

    first commit

--- carrental/vehical-details.php
+++ carrental/vehical-details.php
@@ -2,67 +2,72 @@
 session_start();
 include('includes/config.php');
 error_reporting(0);
+$vhid=intval($POST['id']);
 if(isset($_POST['submit'])) {
+
+  // This is where my code start from
+
   $randomDriver = mt_rand(0, 8);
   // echo "<script>alert('$randomDriver');</script>";
   $selectedDriver = "SELECT `id`, `DriverName`, `DriverPhone` FROM `tbldrivers` WHERE id = $randomDriver";
   $query = $dbh -> prepare($selectedDriver);
-$query->bindParam('id', $id, PDO::PARAM_STR);
-$query->execute();
-$results=$query->fetchAll(PDO::FETCH_OBJ);
-$cnt=1;
-if($query -> rowCount() > 0){
-  foreach($results as $result){ 
-    echo "<script>alert('$result');</script>";
+  $query->bindParam('id', $id, PDO::PARAM_STR);
+  $query->execute();
+  $results=$query->fetchAll(PDO::FETCH_OBJ);
+  $cnt=1;
+  if($query -> rowCount() > 0){
+    foreach($results as $result){ 
+      // echo "<script>alert('$result');</script>";
+    }
   }
+
+  // This is end of my code
+
+$fromdate=$_POST['fromdate'];
+$todate=$_POST['todate']; 
+$message=$_POST['message'];
+$useremail=$_SESSION['login'];
+$status=0;
+$vhid=$_GET['vhid'];
+$bookingno=mt_rand(100000000, 999999999);
+$ret="SELECT * FROM tblbooking where (:fromdate BETWEEN date(FromDate) and date(ToDate) || :todate BETWEEN date(FromDate) and date(ToDate) || date(FromDate) BETWEEN :fromdate and :todate) and VehicleId=:vhid";
+$query1 = $dbh -> prepare($ret);
+$query1->bindParam(':vhid',$vhid, PDO::PARAM_STR);
+$query1->bindParam(':fromdate',$fromdate,PDO::PARAM_STR);
+$query1->bindParam(':todate',$todate,PDO::PARAM_STR);
+$query1->execute();
+$results1=$query1->fetchAll(PDO::FETCH_OBJ);
+
+if($query1->rowCount()==0)
+{
+
+  $sql="INSERT INTO  tblbooking(BookingNumber,userEmail,VehicleId,FromDate,ToDate,message,Status) VALUES(:bookingno,:useremail,:vhid,:fromdate,:todate,:message,:status)";
+  $query = $dbh->prepare($sql);
+  $query->bindParam(':bookingno',$bookingno,PDO::PARAM_STR);
+  $query->bindParam(':useremail',$useremail,PDO::PARAM_STR);
+  $query->bindParam(':vhid',$vhid,PDO::PARAM_STR);
+  $query->bindParam(':fromdate',$fromdate,PDO::PARAM_STR);
+  $query->bindParam(':todate',$todate,PDO::PARAM_STR);
+  $query->bindParam(':message',$message,PDO::PARAM_STR);
+  $query->bindParam(':status',$status,PDO::PARAM_STR);
+  $query->execute();
+  $lastInsertId = $dbh->lastInsertId();
+  if($lastInsertId)
+  {
+  echo "<script>alert('Booking successfull.');</script>";
+  echo "<script type='text/javascript'> document.location = 'my-booking.php'; </script>";
+  }
+  else 
+  {
+  echo "<script>alert('Something went wrong. Please try again');</script>";
+  echo "<script type='text/javascript'> document.location = 'car-listing.php'; </script>";
+  } 
+}  else{
+ echo "<script>alert('Car already booked for these days');</script>"; 
+ echo "<script type='text/javascript'> document.location = 'car-listing.php'; </script>";
 }
-  
+
 }
-// $fromdate=$_POST['fromdate'];
-// $todate=$_POST['todate']; 
-// $message=$_POST['message'];
-// $useremail=$_SESSION['login'];
-// $status=0;
-// $vhid=$_GET['vhid'];
-// $bookingno=mt_rand(100000000, 999999999);
-// $ret="SELECT * FROM tblbooking where (:fromdate BETWEEN date(FromDate) and date(ToDate) || :todate BETWEEN date(FromDate) and date(ToDate) || date(FromDate) BETWEEN :fromdate and :todate) and VehicleId=:vhid";
-// $query1 = $dbh -> prepare($ret);
-// $query1->bindParam(':vhid',$vhid, PDO::PARAM_STR);
-// $query1->bindParam(':fromdate',$fromdate,PDO::PARAM_STR);
-// $query1->bindParam(':todate',$todate,PDO::PARAM_STR);
-// $query1->execute();
-// $results1=$query1->fetchAll(PDO::FETCH_OBJ);
-
-// if($query1->rowCount()==0)
-// {
-
-//   $sql="INSERT INTO  tblbooking(BookingNumber,userEmail,VehicleId,FromDate,ToDate,message,Status) VALUES(:bookingno,:useremail,:vhid,:fromdate,:todate,:message,:status)";
-//   $query = $dbh->prepare($sql);
-//   $query->bindParam(':bookingno',$bookingno,PDO::PARAM_STR);
-//   $query->bindParam(':useremail',$useremail,PDO::PARAM_STR);
-//   $query->bindParam(':vhid',$vhid,PDO::PARAM_STR);
-//   $query->bindParam(':fromdate',$fromdate,PDO::PARAM_STR);
-//   $query->bindParam(':todate',$todate,PDO::PARAM_STR);
-//   $query->bindParam(':message',$message,PDO::PARAM_STR);
-//   $query->bindParam(':status',$status,PDO::PARAM_STR);
-//   $query->execute();
-//   $lastInsertId = $dbh->lastInsertId();
-//   if($lastInsertId)
-//   {
-//   echo "<script>alert('Booking successfull.');</script>";
-//   echo "<script type='text/javascript'> document.location = 'my-booking.php'; </script>";
-//   }
-//   else 
-//   {
-//   echo "<script>alert('Something went wrong. Please try again');</script>";
-//   echo "<script type='text/javascript'> document.location = 'car-listing.php'; </script>";
-//   } 
-// }  else{
-//  echo "<script>alert('Car already booked for these days');</script>"; 
-//  echo "<script type='text/javascript'> document.location = 'car-listing.php'; </script>";
-// }
-
-// }
 
 ?>
 
